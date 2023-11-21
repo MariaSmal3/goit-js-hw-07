dfc5c3cf4a2bab7ff0764d10cc2d459d9aa89979
Maria <smalmaria833@gmail.com>
2023-11-21 11:02:45 +0200

    goit-js-hw-07

---
+++ js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryDiv = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItemsLoad(galleryItems);
+
+galleryDiv.insertAdjacentHTML("beforeend", itemsMarkup);
+
+function createGalleryItemsLoad(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+galleryDiv.addEventListener("click", onImgClick);
+
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+
+  const isItemImage = e.target.classList.contains("gallery__image");
+  if (!isItemImage) return;
+
+  const currentImgUrl = e.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+		<img src="${currentImgUrl}" width="1280" height="auto"/>
+        `,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(e) {
+    if (e.code !== "Escape") return;
+    instance.close();
+  }
+}
